c46af0ea3f7ca184b687db25eaccb17c302b30ba
bargaisabelle <bargaisabelle@gmail.com>
2022-04-20 02:46:43 +0000

    correcting path to csv files

--- Windows/bulk-ou.ps1
+++ Windows/bulk-ou.ps1
@@ -1,4 +1,4 @@
-Import-Csv org-user.csv |
+Import-Csv .\Windows\org-user.csv |
 foreach {
     "New-ADOrganizationalUnit -Name $_.Name -Path $_.Path `
     -ProtectedFromAccidentalDeletion $true -PassThru"
