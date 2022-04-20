cb93d47614c9f232301b8d6f154f550ecb18887f
bargaisabelle <61434443+bargaisabelle@users.noreply.github.com>
2022-04-19 21:14:30 -0400

    create bulk user file

---
+++ Windows/bulk-add.ps1
@@ -0,0 +1,12 @@
+$secpass = Read-Host "Enter a password for all users" -AsSecureString
+
+Import-Csv .\Desktop\new.users.csv |
+foreach {
+    $name = "$($_.FirstName) $($_.LastName)"
+
+    New-ADUser -GivenName $($_.FirstName) -Surname $($_.LastName) `
+    -Name $name -SamAccountName $($_.SamAccountName) `
+    -UserPrincipalName "$($_.SamAccountName)@ad.barga.com" `
+    -AccountPassword $secpass -Path "cn=Users,dc=ad,dc=barga,dc=com" `
+    -Enabled:$false
+}
